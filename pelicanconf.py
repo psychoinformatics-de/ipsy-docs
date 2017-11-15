3cf747b67c073c50693dbb0a7f70fea8ae41d3be
Alex Waite <alex@waite.eu>
2017-11-15 13:46:04 +0100

    Add intra-page headers to Menu nav

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,7 +17,7 @@ LOCALE = u'en_US.UTF-8'
 # Configure Pelican a bit
 #
 PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['pelican-page-hierarchy', 'tipue_search']
+PLUGINS = ['pelican-page-hierarchy', 'pelican-toc', 'tipue_search']
 
 THEME = 'theme'
 DIRECT_TEMPLATES = ['search'] # unset all templates
@@ -25,6 +25,7 @@ STATIC_PATHS = ['css', 'ts']
 
 PATH_METADATA = 'pages/(?P<path>.*)\..*'
 PAGE_ORDER_BY = 'order'
+TOC = { 'TOC_INCLUDE_TITLE': 'false' }
 
 FEED_ALL_ATOM = None
 AUTHOR_SAVE_AS = False
